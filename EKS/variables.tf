c60ee778a833c1caed73fe0bcd91bc61443de5c9
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2021-05-03 17:44:18 -0400

    Update variables.tf

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -7,7 +7,7 @@ variable "cluster-name" {
   type    = string
 }
 variable "key_pair_name" {
-  default = "DevOps"
+  default = "testkey"
 }
 variable "eks_node_instance_type" {
   default = "t2.medium"
